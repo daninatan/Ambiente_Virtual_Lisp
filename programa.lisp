dac5059623bfe7c3252553fd526b7284f0fbe3af
daninatan <danielnatanbrito@hotmail.com>
2025-06-05 15:54:49 -0300

    2 - Adicionando Funções

--- programa.lisp
+++ programa.lisp
@@ -8,7 +8,7 @@
 |#
 
 #|
-Notas: 
+NOTE
   Vou ter que adicionar um menu com as seguintes opções:
 1 - Inicializar Ambiente
   Tamanho do ambiente, sua capacidade total, a temperatura e a umidade
@@ -25,18 +25,25 @@ Notas:
 
 |#
 
-;Tamanho, Capacidade máxima, Temperatura e Umidade
-(setf ambiente (list 0 0 0 0))
+;Tamanho, Capacidade máxima 
 
-(defun main()
-  (menu_principal)
+;IdOrganismo, Posição, Genes, Energia, Idade 
+;Genes - Guardar informações em % da probabilidade dele achar comida, probabilidade de achar comida mais proxima, probabilidade de reproduzir
+
+;IdRecurso, Posição
+
+;Função Principal do programa
+
+(defun MAIN()
+  (let ((ambiente (list 0 0)) (organismos nil) (recursos nil))
+  (menu_principal ambiente organismos recursos))
 )
 
-(defun menu_principal()
+(defun menu_principal(ambiente organismos recursos)
   (let ((escolha 0))
     (loop while(not (equal escolha 8))
       do
-        (format t "1 - Inicializar ambiente~%")
+        (format t "~%~%~%1 - Inicializar ambiente~%")
         (format t "2 - Adicionar Organismo~%")
         (format t "3 - Adicionar Recursos~%")
         (format t "4 - Executar Simulação~%")
@@ -46,24 +53,24 @@ Notas:
         (format t "8 - Sair~%~%")
         (format t "Escolha uma opção: ")
         (setf escolha (read))
-        (setf a 1)
-        (processar_escolha escolha)
+        (setf (values ambiente organismos recursos)
+        (processar_escolha escolha ambiente organismos recursos))
     )
   )
 )
 
 
 
-(defun processar_escolha(escolha)
+(defun processar_escolha(escolha ambiente organismos recursos)
   (cond 
     (
-      (equal escolha 1) (format t "1~%")
+      (equal escolha 1) (inicializar_ambiente ambiente)
     )
     (
-      (equal escolha 2) (format t "2~%") 
+      (equal escolha 2) (adicionar_organismos organismos) 
     )
     (
-      (equal escolha 3) (format t "3~%") 
+      (equal escolha 3) (adicionar_recursos recursos ambiente) 
     )
     (
       (equal escolha 4) (format t "4~%") 
@@ -72,15 +79,70 @@ Notas:
       (equal escolha 5) (format t "5~%") 
     )
     (
-      (equal escolha 6) (format t "6~%") 
+      (equal escolha 6) (estatisticas organismos recursos ambiente) 
     )
     (
-      (equal escolha 7) (format t "7~%") 
+      (equal escolha 7) (resetar) 
     )
     (
-      (equal escolha 8) ()
+      (equal escolha 8) (format t "Saindo do programa...")
     )
   )
+)
+
+(
+  defun inicializar_ambiente(ambiente)
+    (format t "~%~%Qual o tamanho? (ex: 100)  ")
+    (setf (nth 0 ambiente) (read))
+    (format t "~%Qual a capacidade máxima? (ex: 50) ")
+    (setf (nth 1 ambiente) (read))
+ )
+
+
+(
+  defun adicionar_organismos(organismos)
+    (format t "~%~%Qual a quantidade?  ")
+    (setf quantidade (read))
+    (dotimes (i quantidade)
+      (gerar_genes)
+    ) 
+ )
+
+(
+  defun gerar_genes()
+    (setf gene1 (random 100))
+    (print gene1)
+)
+
+;Todas as variáveis se tornam NIL para resetar a simulação
+(defun resetar(organismos ambiente recursos)
+  (setf organismos nil)
+  (setf recursos nil)
+  (setf ambiente nil)
+)
+
+(
+  defun adicionar_recursos(recursos ambiente)
+  (format t "~%~%Qual a quantidade? ")
+  (setf quantidade (read))
+  (dotimes (i quantidade)
+  (setf x (random (nth 0 ambiente)))
+  (setf y (random (nth 0 ambiente)))
+  (push (list x y) recursos))
+)
+
+(
+  defun estatisticas(organismos recursos ambiente)
+    (format t "~%~%Ambiente: ~%")
+    (format t "Tamanho:~A  Capacidade:~A~%~%" (nth 0 ambiente) (nth 1 ambiente))
+    (format t "~%~%Organismos: ~%")
+    (dolist (organismo organismos)
+      (format t "~A~%" organismo)
+    )
+    (format t "~%~%Recursos: ~%")
+    (dolist (recurso recursos)
+      (format t "~A~%" recurso)
+    )
 )
 
 
